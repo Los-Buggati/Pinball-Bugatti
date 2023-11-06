b15afcaa9df643ca01fcd526adea520b1bdaa70a
Lluccib <125250409+Lluccib@users.noreply.github.com>
2023-11-06 11:15:57 +0100

    Update Globals.h

--- Pinball/Globals.h
+++ Pinball/Globals.h
@@ -21,10 +21,10 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 901
-#define SCREEN_HEIGHT 1477
+#define SCREEN_WIDTH 440
+#define SCREEN_HEIGHT 720
 #define SCREEN_SIZE 1
-#define WIN_FULLSCREEN true
+#define WIN_FULLSCREEN false
 #define WIN_RESIZABLE false
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
